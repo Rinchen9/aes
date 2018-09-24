1c96f24fa7394e1faadd62fd57b96782155af6af
Rinchen9 <rinchen@cs.utexas.edu>
2018-09-24 16:06:09 -0500

    Outline of encryption and decryption, notes on 4 steps

---
+++ aes.py
@@ -0,0 +1,11 @@
+def encrypt():
+	# substitute bytes
+	# shift rows
+	# mix columns (don't do for last round)
+	# add round key
+
+def decrypt():
+	# inverse shift rows
+	# inverse substitute bytes
+	# add round key
+	# inverse mixed columns (don't do for last round)
